b59de81ca4aea60aa1c01448d79ff3e73bfec664
Hiep Huynh Van <48771179+HVHMobileDeveloper@users.noreply.github.com>
2020-02-19 02:52:53 +0800

    Add files via upload

---
+++ Compressor.java
@@ -0,0 +1,59 @@
+package com.ims.sentosa.staging.ImageUtils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Compressor {
+    //max width and height values of the compressed image is taken as 612x816
+    private float maxWidth = 1080.0f;
+    private float maxHeight = 1920.0f;
+    private Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.JPEG;
+    private int quality = 80;
+    private String destinationDirectoryPath;
+
+    public Compressor(Context context) {
+        destinationDirectoryPath = context.getCacheDir().getPath() + File.separator + "images";
+    }
+
+    public Compressor setMaxWidth(float maxWidth) {
+        this.maxWidth = maxWidth;
+        return this;
+    }
+
+    public Compressor setMaxHeight(float maxHeight) {
+        this.maxHeight = maxHeight;
+        return this;
+    }
+
+    public Compressor setCompressFormat(Bitmap.CompressFormat compressFormat) {
+        this.compressFormat = compressFormat;
+        return this;
+    }
+
+    public Compressor setQuality(int quality) {
+        this.quality = quality;
+        return this;
+    }
+
+    public Compressor setDestinationDirectoryPath(String destinationDirectoryPath) {
+        this.destinationDirectoryPath = destinationDirectoryPath;
+        return this;
+    }
+
+    public File compressToFile(File imageFile) throws IOException {
+        return compressToFile(imageFile, imageFile.getName());
+    }
+
+    public File compressToFile(File imageFile, String compressedFileName) throws IOException {
+        return ImageUtil.compressImage(imageFile, maxWidth, maxHeight, compressFormat, quality,
+                destinationDirectoryPath + File.separator + compressedFileName);
+    }
+
+    public Bitmap compressToBitmap(File imageFile) throws IOException {
+        return ImageUtil.decodeSampledBitmapFromFile(imageFile, maxWidth, maxHeight);
+    }
+
+}
